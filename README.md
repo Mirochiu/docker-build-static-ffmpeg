c2f0a4a7484ccb7da2662be16da7028b48dba7ab
Chiu, Yung-Hsiang <mirror.tw@gmail.com>
2021-09-03 11:20:35 +0800

    feat: support fdk-aac x265 svtav1 dav1d dash

--- README.md
+++ README.md
@@ -2,24 +2,33 @@ This project is used to build a static version of ffmpeg by docker-compose
 
 1. enter this projet root directory
 
-2. download ffmpeg source code
+2. download source codes
 
 wget -c https://ffmpeg.org/releases/ffmpeg-4.4.tar.gz -O - | tar -xz
-
 or
-
 git clone --depth 1 -b release/4.4 --single-branch https://git.ffmpeg.org/ffmpeg.git ffmpeg-4.4
 
-3. run docker-compose for build
+cd dep
+wget -c https://zlib.net/fossils/zlib-1.2.11.tar.gz -O - | tar -xz
+wget -c ftp://xmlsoft.org/libxml2/libxml2-2.9.2.tar.gz -O - | tar -xz
+wget -c https://github.com/mstorsjo/fdk-aac/archive/refs/tags/v2.0.2.tar.gz -O - | tar -xz
+wget -c https://github.com/videolan/x265/archive/refs/tags/3.4.tar.gz -O - | tar -xz
+wget -c https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v0.8.7/SVT-AV1-v0.8.7.tar.gz -O - | tar -xz
+wget -c https://github.com/numactl/numactl/releases/download/v2.0.14/numactl-2.0.14.tar.gz -O - | tar -xz
+wget -c https://github.com/mesonbuild/meson/archive/refs/tags/0.49.0.tar.gz -O - | tar -xz
+wget -c https://github.com/netwide-assembler/nasm/archive/refs/tags/nasm-2.15.05.tar.gz -O - | tar -xz
+wget -c https://code.videolan.org/videolan/dav1d/-/archive/0.9.1/dav1d-0.9.1.tar.gz -O - | tar -xz
+
+4. run docker-compose for build
 
 docker-compose up --build
 
-4. get the built files
+5. get the built files
 
 out/bin/ffprobe
 out/bin/ffmpeg
 
-5. (optional) try your static build
+6. (optional) try your static build
 
 ./ffprobe -i file:///mnt/hdd2/content/clear.mp4
 ./ffmpeg -i file:///mnt/hdd2/content/clear.mp4 convert.ts
--- build.sh
+++ build.sh
@@ -1,18 +1,103 @@
 #!/bin/bash
 
 echo "workdir:$(pwd) processors:$(nproc) outdir:${OUT_DIR}"
+
+echo "start to build zlib"
+cd /usr/src/dep/zlib-1.2.11
+pwd
+make clean
+./configure --prefix=/usr --static && make -j$(nproc) && make install
+cd -
+
+apt-get -y install \
+  libfreetype6-dev \
+  libvorbis-dev \
+  libx264-dev \
+  libssl-dev \
+  libvpx-dev \
+  libmp3lame-dev \
+  libopus-dev \
+  icu-devtools \
+  libicu-dev \
+  liblzma-dev
+
+echo "start to build xml2"
+cd /usr/src/dep/libxml2-2.9.2
+pwd
+make clean
+./configure --prefix=/usr --enable-static && make -j$(nproc) && make install
+cd -
+
+echo "start to build fdk-aac"
+cd /usr/src/dep/fdk-aac-2.0.2
+pwd
+make clean
+./autogen.sh
+./configure --prefix=/usr && make -j$(nproc) && make install
+cd -
+
+echo "start to build numactl"
+cd /usr/src/dep/numactl-2.0.14
+pwd
+make clean
+./configure --prefix=/usr --enable-static && make -j$(nproc) && make install
+cd -
+
+echo "start to build x265"
+cd /usr/src/dep/x265-3.4
+pwd
+make clean
+cmake ./source -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=OFF && make -j$(nproc) && make install
+cd -
+
+echo "start to build svt-av1"
+cd /usr/src/dep/SVT-AV1-v0.8.7
+pwd
+make clean
+cd Build && cmake .. -G"Unix Makefiles" -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release && make -j$(nproc) && make install
+cd -
+
+apt-get -y install python3 python3-setuptools
+
+echo "start to install meson"
+cd /usr/src/dep/meson-0.49.0
+pwd
+python3 setup.py install
+cd -
+
+apt-get -y remove python3-setuptools
+
+echo "start to install nasm"
+cd /usr/src/dep/nasm-nasm-2.15.05
+pwd
+make distclean
+./autogen.sh
+./configure --prefix=/usr && sed -i -e 393,394d Makefile && make -j$(nproc) && make install
+cd -
+
+echo "start to build libdav1d"
+cd /usr/src/dep/dav1d-0.9.1
+pwd
+rm -rf build
+mkdir build
+meson build --prefix=/usr --buildtype release --default-library=static
+ninja -C build
+ninja -C build install
+cd -
+
+echo "start to build ffmpeg"
+cd /usr/src/ffmpeg
 make distclean
 PKG_CONFIG_PATH="${OUT_DIR}/lib/pkgconfig" ./configure \
   --prefix="${OUT_DIR}" \
   --pkg-config-flags="--static" \
   --extra-cflags="-I${OUT_DIR}/include" \
   --extra-ldflags="-L${OUT_DIR}/lib" \
-  --extra-libs="-lpthread -lm -lz" \
+  --extra-libs="-lpthread -lm -ldl" \
   --extra-ldexeflags="-static" \
   --ld="g++" \
   --enable-gpl \
   --enable-openssl \
-  --enable-libfreetype \
   --enable-libmp3lame \
   --enable-libopus \
   --enable-libvorbis \
@@ -20,20 +105,25 @@ PKG_CONFIG_PATH="${OUT_DIR}/lib/pkgconfig" ./configure \
   --enable-libx264 \
   --enable-nonfree \
   --enable-libfreetype \
+  --enable-libfdk-aac \
+  --enable-libx265 \
+  --enable-libsvtav1 \
+  --enable-libdav1d \
+  --enable-demuxer=dash --enable-libxml2 \
   --disable-doc \
-  --disable-debug \
-  && make -j$(nproc) \
-  && make install
+  --disable-debug
+
+make -j$(nproc) && make install
 
 # cannot install by apt-get
 # --enable-libaom \
-# --enable-libsvtav1 \
-# --enable-libdav1d \
 
-# not found using pkg-config if static build
-# --enable-gnutls \
+# wait for test
+# wget -c https://github.com/libass/libass/releases/download/0.15.1/libass-0.15.1.tar.gz -O - | tar -xz
 # --enable-libass \
-# --enable-libx265 \
-# --enable-libfdk-aac \
+
+# got error when configure
+# /usr/bin/ld: attempted static link of dynamic object `/usr/lib/x86_64-linux-gnu/libz.so'
+# --enable-gnutls \
 # --enable-librtmp \
-# --enable-demuxer=dash --enable-libxml2 \
+
